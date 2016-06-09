94d7c6971192fd35a7b263002711750288694a30
Quan Wang <BetterWang@gmail.com>
2016-06-09 12:42:57 +0200

    [+] label

--- label.h
+++ label.h
@@ -4,16 +4,18 @@
 char const * fname[] = {
 	"test/",									// 0
 	"../QVector/HIMinimumBias3/crab_HIMB3_QVector_v5/160501_180514/0000/",		// 1 1-3 GeV/c
+	"../QVector/HIMinimumBias3/crab_HIMB3_QVector_randq_v1/160608_174408/0000/",	// 2 randq 1-3 GeV/c
 };
 
 char const * ftxt[] = {
 	"test/",								// 0
 	"txt/HIMB3_v1/",							// 1
+	"txt/HIMB3_randq_v1/",							// 2
 };
 
 TChain * chV = new TChain("trV");
 void addchain(int s1)
 {
 	std::cout << fname[s1] << std::endl;
-	chV->Add(Form("%s/qvector_*.root/QVector/trV", fname[s1]));
+	chV->Add(Form("%s/qvector*.root/QVector/trV", fname[s1]));
 }
