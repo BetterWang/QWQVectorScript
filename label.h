2354ebbe38510abd15e726d5056627d174bb8781
Quan Wang <BetterWang@gmail.com>
2016-06-17 15:16:47 +0200

    [+] label

--- label.h
+++ label.h
@@ -11,7 +11,10 @@ char const * fname[] = {
 	"../QVector/HIMinimumBias4/crab_HIMB4_QVector_v5/160616_103838/0000/",		// 6 HIMB4 1-3 GeV/c
 	"NA",										// 7 merged 1,4,5,6 HIMB[1,2,3,4] 1-3 GeV/c
 	"../QVector/Hydjet_Quenched_MinBias_5020GeV_750/crab_Hydjet_QVector_v5/160616_192548/0000/",		// 8 Hydjet 1-3 GeV/c
-	"",		// 9 Hydjet randq 1-3 GeV/c
+	"../QVector/Hydjet_Quenched_MinBias_5020GeV_750/crab_Hydjet_QVector_randq_v1/160617_094658/0000/",	// 9 Hydjet randq 1-3 GeV/c
+	"../QVector/HIMinimumBias1/crab_HIMB1_QVector_randq_v1/160617_090018/0000/",	// 10 HIMB1 randq 1-3 GeV/c
+	"../QVector/HIMinimumBias2/crab_HIMB2_QVector_randq_v1/160617_090032/0000/",	// 11 HIMB2 randq 1-3 GeV/c
+	"../QVector/HIMinimumBias4/crab_HIMB4_QVector_randq_v1/160617_090047/0000/",	// 12 HIMB4 randq 1-3 GeV/c
 };
 
 char const * ftxt[] = {
@@ -25,6 +28,9 @@ char const * ftxt[] = {
 	"txt/HIMB_1234_v1/",							// 7
 	"txt/Hydjet_v1/",							// 8
 	"txt/Hydjet_randq_v1/",							// 9
+	"txt/HIMB1_randq_v1/",							// 10
+	"txt/HIMB2_randq_v1/",							// 11
+	"txt/HIMB4_randq_v1/",							// 12
 };
 
 TChain * chV = new TChain("trV");
